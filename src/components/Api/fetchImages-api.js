59745368dbeab15fcb2d2172da9d3a342b4f6846
vButkevich <vyacheslav.butkevich@gmail.com>
2024-05-26 14:07:05 +0300

    goit-react-hw-04

--- src/components/Api/fetchImages-api.js
+++ src/components/Api/fetchImages-api.js
@@ -2,9 +2,6 @@
 import axios from "axios";
 
 export const fetchImagesByQuery = async ({ query, page, pageItems = 15 }) => {
-  console.log(`pixabay:search=>q:[${query}]`);
-  console.log(`pixabay:search=>p:[${page}]`);
-
   const baseUrl = "https://pixabay.com/api/";
   const api_key = "42700764-e859e8fc9280be2dbaa9956c4";
   const queryParams = {
@@ -23,9 +20,6 @@ export const fetchImagesByQuery = async ({ query, page, pageItems = 15 }) => {
     const response = await axios.get(url, config);
     return response;
   } catch (error) {
-    console.error("pixabay:Error fetching images:", error);
     throw error;
-  } finally {
-    console.log(`pixabay:search.end;`);
   }
 };
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -12,7 +12,6 @@ import LoadMoreBtn from "../LoadMoreBtn/LoadMoreBtn";
 import ImageModal from "../ImageModal/ImageModal";
 
 import { fetchImagesByQuery } from "../Api/fetchImages-api";
-import { generateId } from "../Api/genarateId";
 
 function App() {
   const [page, setPage] = useState(0);
@@ -23,11 +22,7 @@ function App() {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  console.log("App.query:", query);
-  console.log("App.id:", generateId());
-
   const handleSearch = (serchQuery) => {
-    console.log("App.getQuery", query);
     setQuery(serchQuery);
     setError(null);
     setImages([]);
@@ -35,12 +30,10 @@ function App() {
   };
 
   const loadMore = () => {
-    console.log("loadMore:", page);
     setPage((page) => page + 1);
   };
 
   const openModal = (image) => {
-    console.log("image :>> ", image);
     setModalImage(image);
   };
 
@@ -48,9 +41,8 @@ function App() {
     setModalImage(null);
   };
 
-  const itemsPerPage = 11;
+  const itemsPerPage = 15;
   useEffect(() => {
-    console.log("useEffect(() => ", query);
     if (!query) return;
     async function getImages() {
       try {
@@ -61,12 +53,9 @@ function App() {
           itemsPerPage,
         });
         const images = response.data.hits;
-        // console.log("response:", response);
-        // console.log("data:", response.data);
-        // console.log("images:", images);
 
         setImages((prevImages) => [...prevImages, ...images]);
-        setHasMore(response.data.totalHits >= page * itemsPerPage);
+        setHasMore(response.data.totalHits > page * itemsPerPage);
       } catch (error) {
         setError(error.message);
       } finally {
@@ -75,7 +64,6 @@ function App() {
     }
     getImages();
   }, [query, page]);
-
 
   return (
     <>
